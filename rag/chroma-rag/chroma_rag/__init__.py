bf9e76c519d8db1421f0de91732dde04377b165b
Erick Friis <erick@langchain.dev>
2023-10-12 16:12:24 -0700

    chroma

--- rag/chroma-rag/chroma_rag/__init__.py
+++ rag/chroma-rag/chroma_rag/__init__.py
@@ -4,10 +4,10 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.vectorstores import FAISS
+from langchain.schema.runnable import RunnablePassthrough, RunnableParallel
+from langchain.vectorstores import Chroma
 
-vectorstore = FAISS.from_texts(
+vectorstore = Chroma.from_texts(
     ["harrison worked at kensho"], embedding=OpenAIEmbeddings()
 )
 retriever = vectorstore.as_retriever()
@@ -22,7 +22,7 @@ prompt = ChatPromptTemplate.from_template(template)
 model = ChatOpenAI()
 
 chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
+    RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
     | prompt
     | model
     | StrOutputParser()
