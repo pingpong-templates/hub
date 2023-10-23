310b42eda0bf3661e35f007c611e8f01dc281b8b
Lance Martin <lance@langchain.dev>
2023-10-23 14:16:32 -0700

    Anthropic function calling

---
+++ anthropic-function/anthropic_function/chain.py
@@ -0,0 +1,16 @@
+from langchain.chains import create_tagging_chain
+from langchain_experimental.llms.anthropic_functions import AnthropicFunctions
+
+model = AnthropicFunctions(model='claude-2')
+
+schema = {
+    "properties": {
+        "sentiment": {"type": "string"},
+        "aggressiveness": {"type": "integer"},
+        "language": {"type": "string"},
+    }
+}
+
+chain = create_tagging_chain(schema, model)
+
+
