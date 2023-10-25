ce221e1a7cc633c50ebb71fece292fc368348ea6
Lance Martin <lance@langchain.dev>
2023-10-25 12:23:18 -0700

    Bump LangChain version for extraction template

--- openai-functions/openai_functions/chain.py
+++ openai-functions/openai_functions/chain.py
@@ -11,7 +11,7 @@ class Overview(BaseModel):
 
 # Function definition
 model = ChatOpenAI()
-overview_extraction_function = [convert_pydantic_to_openai_function(Overview)]
+function = [convert_pydantic_to_openai_function(Overview)]
 chain = model.bind(
-    functions=overview_extraction_function, function_call={"name": "Overview"}
+    functions=function, function_call={"name": "Overview"}
 ).with_types(input_type=str)
